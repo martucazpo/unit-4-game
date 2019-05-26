cc396ae2e8e09d60fa16624361abc402af5a3ef6
MarcoTucsonpo <martucazpo@gmail.com>
2019-05-26 09:44:46 -0700

    the fn butns doing sumThing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,52 @@
 var randNum = Math.floor(Math.random() * (150 - 50 + 1) + 50);
 var crysVal = [];
+var crysNum = [];
 var playGame = [];
+var crys1 = [];
+var crys2 = [];
+var crys3 = [];
+var crys4 = [];
 wins = 0;
 losses = 0;
-display = "";
 
 $(document).ready(function () {
     $("#randNum").html(randNum);
 });
 
-$(document).ready(crysNum);
+$(document).ready(getNum);
 
-function crysNum() {
+function getNum() {
     while (crysVal.length < 4) {
         var r = Math.ceil(Math.random() * (20 - 1 + 1) + 1);
         if (crysVal.indexOf(r) === -1) crysVal.push(r);
         console.log(crysVal);
-        $(document).on("click", function () {
+
+        $("#crys1").on("click", function () {
             $("#crys1").html(crysVal[0]);
+            crys1 = crysVal[0]; 
+            playGame.push(crys1);
+            console.log(playGame);
+        });
+        $("#crys2").on("click", function () {
             $("#crys2").html(crysVal[1]);
+            crys2 = crysVal[1];
+            playGame.push(crys2);
+            console.log(playGame);
+        });
+        $("#crys3").on("click", function () {
             $("#crys3").html(crysVal[2]);
-            $("#crys4").html(crysVal[3]);
-                         
-            $("#crys1").on("click", function () {
-                playGame.push(crysVal[0]);
-            });
-            $("#crys2").on("click", function () {
-                playGame.push(crysVal[1]);
-            });
-            $("#crys3").on("click", function () {
-                playGame.push(crysVal[2]);
-            });
-            $("#crys4").on("click", function () {
-                playGame.push(crysVal[3]);
-            });
+            crys3 = crysVal[2];
+            playGame.push(crys3);
             console.log(playGame);
 
-        })
-
+        });
+        $("#crys4").on("click", function () {
+            $("#crys4").html(crysVal[3]);
+            crys4 = crysVal[3];
+            playGame.push(crys4);
+            console.log(playGame);
+        });
+        console.log(playGame);
     };
+    
 };
